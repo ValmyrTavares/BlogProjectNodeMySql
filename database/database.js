debb9305a52b3423ed6bcb34150b75a471c65b6d
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-08-11 17:24:25 -0300

    Mandando o artigo para o banco de dados, atualizando a hora do Brasil e relacionado o artigo com a categoria do select

--- database/database.js
+++ database/database.js
@@ -1,7 +1,9 @@
 const Sequelize = require('sequelize')
 const connection = new Sequelize('guiapress', 'root','Amanda@230406',{
     host:'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00"
+
 })
 
 module.exports = connection;
