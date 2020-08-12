78591cc81a3d24933b7dde2ee8564f508ea5371d
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-08-12 16:31:41 -0300

    Aula 109 Consegundo mandar a atualização dos artigos para o banco de dados

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -67,5 +67,21 @@ router.get("/admin/articles/edit/:id", (req, res)=>{
     })
 })
 
+router.post("/articles/update",(req, res)=>{
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+    Article.update({title:title, id:id, body:body, categoryId:category},{
+        where:{
+            id:id
+        }
+    }).then(()=>{
+        res.redirect("/admin/articles")
+    }).catch((err)=>{
+        res.redirect("/")
+    })
+})
+
 
 module.exports =  router;
