70cc8bdd73b9b4e3bd7fe60dbb90702120c89765
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-08-12 18:27:15 -0300

    Aula 112 paginação quase concluida

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -83,5 +83,37 @@ router.post("/articles/update",(req, res)=>{
     })
 })
 
+router.get("/articles/page/:num",(req, res) =>{
+    var page = req.params.num;
+    var offset = 0
+
+    if(isNaN(page) || page==1){
+        offset = 0;
+    }else{
+        offset = parseInt(page) * 4
+    }
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset,
+        order:[
+            ['id','DESC']
+        ]
+    }).then(articles =>{        
+        var next;
+        if(offset + 4 >= articles.count){
+            next = false
+        }else{
+            next= true
+        }
+        var result = {
+            next:next,
+            articles: articles
+        }
+        Category.findAll().then(categories =>{
+            res.render("admin/articles/pages", {result:result,categories:categories})
+        })    
+    })
+})
+
 
 module.exports =  router;
